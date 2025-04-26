d12a4152611fd01bf65bc3587e4170e41e9e0313
Chinmay-innovates <chinmaygope69@gmail.com>
2025-04-26 16:00:26 +0530

    Refactor imports, enhance BookOverview component, and add constants for navigation and book data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import { BookList } from "~/components/book-list";
 import { BookOverview } from "~/components/book-overview";
-import { sampleBooks } from "../constants";
+import { sampleBooks } from "~/constants";
 
 const Home = () => {
 	return (
